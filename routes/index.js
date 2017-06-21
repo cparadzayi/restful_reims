28984ed1832e804887475f27b256f712347a8bed
DESKTOP-Q6I58F0\Charles Paradzayi <cparadzayi@gmail.com>
2017-06-21 19:57:26 +0200

    changed tis to this in the routes index.js

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/secret', passport.authenticate('jwt', { session: false }), (req, res) => {
-	res.json('The fact that you saw tis means you are authenticated')
+	res.json('The fact that you saw this means you are authenticated')
 })
 
 module.exports = router
